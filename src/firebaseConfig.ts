5394d5ff609d22e989b257b16e17ec82960a6d22
chajaesik01 <ckwotlr@dongguk.edu>
2024-12-02 11:46:20 +0900

    settings: review edit

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,3 +1,8 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+import { getStorage } from 'firebase/storage';
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -8,4 +13,11 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-export default firebaseConfig;
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const database = getDatabase(app);
+const storage = getStorage(app);
+
+export { auth, database, storage };
+export default app;
