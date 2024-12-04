0def4b1b8412ba3ebf9f95c23953aa4d7ed5ac27
gold <goldegg127@gmail.com>
2024-12-04 16:37:31 +0900

    feat: 디자인 토큰 border, font, padding 영역 모듈화 (#7)

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,52 +1,11 @@
 import { colors } from './token/colors';
+import { padding } from './token/padding';
+import { font } from './token/font';
+import { border } from './token/border';
 
 export const styles = {
-  padding: {
-    xs: '4px',
-    sm: '8px',
-    md: '16px',
-    lg: '36px',
-    xl: '48px',
-    values: {
-      xs: 4,
-      sm: 8,
-      md: 16,
-      lg: 36,
-      xl: 48,
-    },
-  },
-  font: {
-    size: {
-      heading: '36px',
-      subHeading: '24px',
-      paragraph: '16px',
-      values: {
-        heading: 36,
-        subHeading: 24,
-        paragraph: 16,
-      },
-    },
-    family: "'Noto Sans KR', sans-serif",
-    weight: {
-      heading: 300,
-    },
-  },
-  border: {
-    widthValue: 1,
-    width: '1px',
-    style: 'solid',
-    color: colors.semantic.border,
-    default: colors.semantic.border,
-    radius: {
-      xs: '4px',
-      lg: '32px',
-      values: {
-        xs: 4,
-        lg: 32,
-      },
-    },
-  },
-  media: {
-    widthScreen: 1024,
-  },
+  padding,
+  font,
+  border,
+  colors,
 };
---
+++ src/styles/token/border.ts
@@ -0,0 +1,17 @@
+import { colors } from './colors';
+
+export const border = {
+  widthValue: 1,
+  width: '1px',
+  style: 'solid',
+  color: colors.semantic.border,
+  default: colors.semantic.border,
+  radius: {
+    xs: '4px',
+    lg: '32px',
+    values: {
+      xs: 4,
+      lg: 32,
+    },
+  },
+};
