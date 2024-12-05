796ae58c5af97469793ba2dc256621f4ad2f8fcc
gold <goldegg127@gmail.com>
2024-12-05 16:22:19 +0900

    feat: redux 사용예시를 위해 store, actions, dispatch를 사용하는 Counter 컴포넌트 생성 (#26)

---
+++ src/components/Counter.tsx
@@ -0,0 +1,18 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../state/store';
+import { increment, decrement } from '../slices/counter/actions';
+
+const Counter = () => {
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>{count}</div>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
