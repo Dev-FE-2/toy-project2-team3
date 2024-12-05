09190fb065ee18e29a76a8fd6e78eb52346b2d45
vgotu99 <vgotu99@gmail.com>
2024-12-05 22:31:31 +0900

    feat: MonthPicker의 년/월 선택 기능 구현 (#23)

--- src/pages/user/Schedule/ScheduleHeader/index.tsx
+++ src/pages/user/Schedule/ScheduleHeader/index.tsx
@@ -1,10 +1,41 @@
 import styled from 'styled-components';
 import MonthPicker from './MonthPicker';
+import { useState } from 'react';
+import MonthPickerDetail from './MonthPickerDetail';
 
-const ScheduleHeader = () => {
+interface ScheduleHeaderProps {
+  currentMonth: number;
+  currentYear: number;
+  handleYearMonthChange: (year: number, month: number) => void;
+}
+
+const ScheduleHeader = ({
+  currentMonth,
+  currentYear,
+  handleYearMonthChange,
+}: ScheduleHeaderProps) => {
+  const [isMonthPickerDetailOpen, setIsMonthPickerDetailOpen] = useState(false);
+
+  const handleMonthClick = (clickedMonth: number) => {
+    handleYearMonthChange(currentYear, clickedMonth + 1);
+    setIsMonthPickerDetailOpen(false)
+  };
+
   return (
     <StyledHeader>
-      <MonthPicker />
+      <MonthPicker
+        currentMonth={currentMonth}
+        currentYear={currentYear}
+        handleYearMonthChange={handleYearMonthChange}
+        isMonthPickerDetailOpen={isMonthPickerDetailOpen}
+        setIsMonthPickerDetailOpen={setIsMonthPickerDetailOpen}
+      />
+      {isMonthPickerDetailOpen && (
+        <MonthPickerDetail
+          currentMonth={currentMonth}
+          handleMonthClick={handleMonthClick}
+        />
+      )}
     </StyledHeader>
   );
 };
--- src/pages/user/Schedule/index.tsx
+++ src/pages/user/Schedule/index.tsx
@@ -3,15 +3,31 @@ import ScheduleHeader from './ScheduleHeader';
 import ScheduleMain from './ScheduleMain';
 import ScheduleSideBar from './ScheduleSideBar';
 import TempScheduleCreator from './TempScheduleCreator';
+import { useState } from 'react';
 
 const Schedule = () => {
+  const CURRENT_MONTH = new Date().getMonth() + 1;
+  const CURRENT_YEAR = new Date().getFullYear();
+  const [currentMonth, setCurrentMonth] = useState(CURRENT_MONTH);
+  const [currentYear, setCurrentYear] = useState(CURRENT_YEAR);
+
+  const handleYearMonthChange = (year: number, month: number) => {
+    setCurrentYear(year);
+    setCurrentMonth(month);
+  };
+
   return (
     <>
       <StyledWrapper>
         <ScheduleSideBar />
         <div>
-          <ScheduleHeader />
-          <ScheduleMain />
+          <ScheduleHeader
+            currentMonth={currentMonth}
+            currentYear={currentYear}
+            handleYearMonthChange={handleYearMonthChange}
+          />
+          <ScheduleMain
+          />
         </div>
       </StyledWrapper>
       <TempScheduleCreator />
