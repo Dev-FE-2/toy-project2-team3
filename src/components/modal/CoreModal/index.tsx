f1e23cd1865bd9aeded0ff8e70588503ea5e9051
Chajaesik01 <127061507+Chajaesik01@users.noreply.github.com>
2024-12-06 18:22:14 +0900

    [Design] figma에 정의된 modal UI 구현 (#29)
    * design: figma에 정의된 modal UI 구현 [#20]
    
    * fix: pr 리뷰 반영

---
+++ src/components/modal/CoreModal/index.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import ErrorModal from '../CoreModal/ErrorModal';
+import QuestionModal from '../CoreModal/QuestionModal';
+import SuccessModal from '../CoreModal/SuccessModal';
+import { colors, font } from '../../../styles';
+
+type CoreModalProps = {
+  modalType: 'check' | 'error' | 'question';
+  modalMessage: string;
+  onClose: () => void;
+};
+
+export type ModalProps = {
+  onClose: () => void;
+  ModalMessage: string;
+};
+
+// CoreModal 컴포넌트
+const CoreModal: React.FC<CoreModalProps> = ({
+  modalType,
+  modalMessage,
+  onClose,
+}) => {
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
+
+  return (
+    <StyledModalBackground onClick={onClose}>
+      <StyledModalContainer onClick={(e) => e.stopPropagation()}>
+        {modalType === 'check' ? (
+          <SuccessModal onClose={onClose} ModalMessage={modalMessage} />
+        ) : modalType === 'error' ? (
+          <ErrorModal onClose={onClose} ModalMessage={modalMessage} />
+        ) : modalType === 'question' ? (
+          <QuestionModal onClose={onClose} ModalMessage={modalMessage} />
+        ) : (
+          <div> No type Modal </div>
+        )}
+      </StyledModalContainer>
+    </StyledModalBackground>
+  );
+};
+
+// Styled 컴포넌트
+const StyledModalBackground = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledModalContainer = styled.div`
+  background-color: ${colors.semantic.background.light};
+  border-radius: 8px;
+  border: 0.5px solid ${colors.semantic.primary};
+  width: 26.67vw;
+  height: 23.7vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 1.5vh;
+  img {
+    width: 3.33vw;
+    height: auto;
+  }
+  font-size: ${font.size.subHeading};
+  font-weight: 700;
+`;
+
+export default CoreModal;
