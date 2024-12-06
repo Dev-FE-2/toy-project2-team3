9d93248e384ca08996c0e891c5a07a3aae1d89f5
goldegg127 <goldegg127@gmail.com>
2024-12-06 15:57:18 +0900

    [Refactor] 회원가입 페이지, 로그인 페이지를 묶는 디렉토리 생성 및 파일 구조 수정합니다 (#40)
    * refactor: sign 디렉토링 생성하여 SignUp, Login 페이지컴포넌트 파일 그룹화 (#38)
    
    * refactor: pages의 배럴 export 구조 수정 (#38)

--- src/pages/Login/index.tsx
+++
@@ -1,4 +0,0 @@
-function Login() {
-  return <h1>로그인 페이지입니다.</h1>;
-}
-export default Login;
--- src/pages/sign/SignUp/index.tsx
+++ src/pages/sign/SignUp/index.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import { default as SignUpForm } from './SignUpForm';
 
-function SignUp() {
+const SignUp = () => {
   return (
     <div>
       <h1>회원가입 페이지입니다.</h1>
+      <SignUpForm />
     </div>
   );
-}
+};
 
 export default SignUp;
