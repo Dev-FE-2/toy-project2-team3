2a6ca2c19c3ce7e5ba46d2664ddce05041b2d297
gold <goldegg127@gmail.com>
2024-12-07 19:53:20 +0900

    settings: 절대경로 별칭(path alias) 설정을 위해 vite.config.ts 파일 업데이트 (#42)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,47 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
+const BASE_DIR = 'src';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    alias: [
+      {
+        find: 'public',
+        replacement: path.resolve(__dirname, 'public'),
+      },
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, BASE_DIR),
+      },
+      {
+        find: '@assets',
+        replacement: path.resolve(__dirname, `${BASE_DIR}/assets`),
+      },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, `${BASE_DIR}/components`),
+      },
+      {
+        find: '@slices',
+        replacement: path.resolve(__dirname, `${BASE_DIR}/slices`),
+      },
+      {
+        find: '@store',
+        replacement: path.resolve(__dirname, `${BASE_DIR}/state/store`),
+      },
+      {
+        find: '@styles',
+        replacement: path.resolve(__dirname, `${BASE_DIR}/styles`),
+      },
+      {
+        find: '@types',
+        replacement: path.resolve(__dirname, `${BASE_DIR}/types`),
+      },
+    ],
+  },
 });
