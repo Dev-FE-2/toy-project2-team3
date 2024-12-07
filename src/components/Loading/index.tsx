d3d5d291d2281d89efdece7dcc4b27523dc31822
gold <goldegg127@gmail.com>
2024-12-07 17:48:13 +0900

    feat: 로딩상태 UI를 위한 Loading 컴포넌트 생성 (#37)

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,41 @@
+import { styled, keyframes } from 'styled-components';
+import { padding, colors } from '../../styles';
+
+const Loading = () => {
+  return (
+    <S.SpinnerContainer>
+      <div className="spinner"></div>
+    </S.SpinnerContainer>
+  );
+};
+
+const S = {
+  SpinnerContainer: styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: ${padding.xl};
+
+    .spinner {
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      border: 5px solid ${colors.semantic.border};
+      border-top: 5px solid ${colors.semantic.tertiary};
+      animation: spin 1s linear infinite;
+    }
+  `,
+  spin: keyframes`
+        0% {
+            transform: rotate(0deg);
+        }
+
+        100% {
+            transform: rotate(360deg);
+        }
+    `,
+};
+
+export default Loading;
