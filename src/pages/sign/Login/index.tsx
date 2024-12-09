77dc7579648526bcac87b95a2df93be09c290bd1
gold <goldegg127@gmail.com>
2024-12-10 02:25:59 +0900

    test: 사용자 목록을 받아오는 useFetchUsers 훅을 사용해 UI 렌더링 (#37)

--- src/pages/sign/Login/index.tsx
+++ src/pages/sign/Login/index.tsx
@@ -1,10 +1,29 @@
+import { useFetchUsers } from '../../../hooks';
 import { default as LoginForm } from './LogInForm';
+import { default as Loading } from '../../../components/Loading';
 
 const Login = () => {
+  const { data, error, isLoading } = useFetchUsers();
+
+  if (isLoading)
+    return (
+      <div>
+        <Loading />
+      </div>
+    );
+  if (error) return <div>오류 발생: {error.message}</div>;
+
   return (
     <section>
       <h1>로그인</h1>
       <LoginForm />
+      <ul>
+        {data.length > 0 ? (
+          data.map((user) => <li key={user.userId}>{user.name}</li>)
+        ) : (
+          <li>데이터가 없습니다.</li>
+        )}
+      </ul>
     </section>
   );
 };
