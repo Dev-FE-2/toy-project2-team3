04bca3824f148a29de89c162a8f63c08f4aa3526
gold <goldegg127@gmail.com>
2024-12-09 14:35:20 +0900

    feat: swr config를 App 상위 컴포넌트에 설정 (#37)

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,27 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './state/store.ts';
+import { SWRConfig } from 'swr';
+import store from '@store';
 import App from './App.tsx';
 import './index.css';
+
+const fetcher = (...args: Parameters<typeof fetch>) =>
+  fetch(...args).then((res) => res.json());
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <SWRConfig
+        value={{
+          fetcher,
+          revalidateOnFocus: false, // 포커스 시 재검증 비활성화
+          shouldRetryOnError: true, // 오류 시 자동 재시도 활성화
+          errorRetryInterval: 3000, // 재시도 간격
+        }}
+      >
+        <App />
+      </SWRConfig>
     </Provider>
   </StrictMode>
 );
