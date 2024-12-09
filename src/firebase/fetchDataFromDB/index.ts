b5b071fe7996d0e9ea7c429c881726a814358948
vgotu99 <vgotu99@gmail.com>
2024-12-09 11:44:42 +0900

    feat: 데이터를 가져오는 유틸 함수 구현 (#44)

---
+++ src/firebase/fetchDataFromDB/index.ts
@@ -0,0 +1,32 @@
+import { ref, get, DatabaseReference } from 'firebase/database';
+import { database } from '../../firebaseConfig';
+
+export async function fetchDataFromDB<T>(
+  collectionName: string,
+  docName: string | null = null
+): Promise<T | Record<string, T> | null> {
+  try {
+    const dbRef: DatabaseReference = docName
+      ? ref(database, `${collectionName}/${docName}`)
+      : ref(database, collectionName);
+
+    const snapshot = await get(dbRef);
+    if (snapshot.exists()) {
+      return snapshot.val();
+    } else {
+      console.warn('데이터가 존재하지 않습니다.');
+      return null;
+    }
+  } catch (error) {
+    console.error('데이터 가져오기 실패:', error);
+    throw error;
+  }
+}
+
+// 사용방법
+/*
+  import fetchDataFromDB from '~~~~'
+
+  const USER_DATA = fetchDataFromDB('user', docName)
+  // docName은 문서의 이름 혹은 null이 들어갈 수 있다.
+*/
