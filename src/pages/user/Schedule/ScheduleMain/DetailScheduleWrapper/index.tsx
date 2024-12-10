9fb39469ac23ccd917f62d17528c90ce63e54287
vgotu99 <vgotu99@gmail.com>
2024-12-10 16:39:10 +0900

    feat: 상세 일정에 현재 시간을 가시적으로 표현하는 타임라인 막대 추가(#47)

--- src/pages/user/Schedule/ScheduleMain/DetailScheduleWrapper/index.tsx
+++ src/pages/user/Schedule/ScheduleMain/DetailScheduleWrapper/index.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { border, colors } from '../../../../../styles';
 import { TEAMS } from '../../constants';
 
 const DetailScheduleWrapper: React.FC = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
   const TEMP_TEAM_MEMBERS = TEAMS[0].members;
-
   const TEAM_MEMBERS_LENGTH = TEMP_TEAM_MEMBERS.length;
+  const HOURS = currentTime.getHours();
+  const MINUTES = currentTime.getMinutes();
+  const TOTAL_MINUTES = HOURS * 60 + MINUTES;
+  const TIME_LINE_POSITION = (TOTAL_MINUTES / 60) * 80 + 40;
+
+  useEffect(() => {
+    const getCurrentTime = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 60000);
+
+    return () => clearInterval(getCurrentTime);
+  }, []);
 
   const generateCells = (teamMembers: string[]) => {
     return Array.from({ length: 24 }, (_, hour) => ({
@@ -32,6 +44,7 @@ const DetailScheduleWrapper: React.FC = () => {
             ))}
           </S.CellsContainer>
         ))}
+        <S.CurrentTimeLine style={{ top: `${TIME_LINE_POSITION}px` }} />
       </S.ScheduleContainer>
     </>
   );
@@ -40,7 +53,6 @@ const DetailScheduleWrapper: React.FC = () => {
 const S = {
   ScheduleContainer: styled.div<{ teamMembersLength: number }>`
     width: 1250px;
-    height: 100%;
     max-height: calc(80dvh - 3rem);
     display: grid;
     grid-template-columns: auto repeat(
@@ -50,8 +62,10 @@ const S = {
     grid-template-rows: auto repeat(24, 1fr);
     border: ${border.default};
     overflow-y: auto;
+    position: relative;
   `,
   HeaderCell: styled.div`
+    /* box-sizing: border-box; */
     background-color: ${colors.semantic.background.light};
     border: ${border.default};
     display: flex;
@@ -59,7 +73,7 @@ const S = {
     justify-content: center;
     font-size: 1rem;
     font-weight: bold;
-    height: 3rem;
+    height: 40px;
     position: sticky;
     top: 0;
     z-index: 2;
@@ -75,7 +89,7 @@ const S = {
     font-size: 14px;
   `,
   TimeCell: styled.div`
-    height: 6rem;
+    height: 80px;
     width: 3rem;
     border: ${border.default};
     display: flex;
@@ -84,6 +98,15 @@ const S = {
     font-size: 14px;
     font-weight: bold;
     background-color: ${colors.semantic.background.light};
+  `,
+  CurrentTimeLine: styled.div`
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 1240px;
+    height: 2px;
+    background-color: ${colors.semantic.danger};
+    z-index: 5;
   `,
 };
 export default DetailScheduleWrapper;
