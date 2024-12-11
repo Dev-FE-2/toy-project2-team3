222a0d261c612329ebb86192fb41484b3c900320
vgotu99 <vgotu99@gmail.com>
2024-12-12 01:30:56 +0900

    feat: 팀 스케줄 데이터 포맷팅 (#60)

--- src/pages/user/Schedule/ScheduleMain/DetailScheduleWrapper/DetailSchedule/index.tsx
+++ src/pages/user/Schedule/ScheduleMain/DetailScheduleWrapper/DetailSchedule/index.tsx
@@ -1,4 +1,26 @@
 import styled from 'styled-components';
+import { fetchDataFromDB } from '../../../../../../firebase/fetchDataFromDB';
+import { useCallback, useEffect, useState } from 'react';
+
+interface ScheduleList {
+  createdAt: string;
+  detail: string;
+  endedAt: string;
+  startedAt: string;
+  title: string;
+  updatedAt: string;
+}
+
+interface ScheduleData {
+  id: string;
+  scheduleList: ScheduleList[];
+  userId: string;
+}
+
+interface FormattedUserOrTeamScheduleData extends ScheduleData {
+  type: string;
+  name: string;
+}
 
 interface TeamMembersData {
   name: string;
@@ -20,9 +42,64 @@ const DetailSchedule = ({
   currentSchedule,
   clickedDate,
 }: DetailSchedulProps) => {
-  console.log(currentSchedule);
-  console.log(clickedDate);
+  const [teamScheduleData, setTeamScheduleData] = useState<
+    FormattedUserOrTeamScheduleData[]
+  >([]);
 
+  console.log(clickedDate); // 삭제 예정
+  console.log(teamScheduleData); // 삭제 예정
+
+  const getScheduleData = async () => {
+    const scheduleData = await fetchDataFromDB('Schedule');
+
+    const formattedScheduleData: ScheduleData[] = scheduleData
+      ? Object.entries(scheduleData).map(([id, scheduleData]) => ({
+          id,
+          scheduleList: scheduleData.scheduleList,
+          userId: scheduleData.userId,
+        }))
+      : [];
+
+    return formattedScheduleData;
+  };
+
+  const formatTeamSchedule = (
+    currentSchedule: CurrentSchedule,
+    scheduleData: ScheduleData[]
+  ) => {
+    const teamMembersUserId = currentSchedule.teamId.map((id) => id.userId);
+    const teamScheduleData = scheduleData.filter((schedule) =>
+      teamMembersUserId.includes(schedule.userId)
+    );
+
+    const formattedTeamSchedule = teamScheduleData.map((schedule) => {
+      const teamMemberInfo = currentSchedule.teamId.find(
+        (info) => info.userId === schedule.userId
+      );
+
+      return {
+        ...schedule,
+        type: currentSchedule.type,
+        name: teamMemberInfo ? teamMemberInfo.name : '',
+      };
+    });
+
+    return formattedTeamSchedule;
+  };
+
+  const fetchSchedules = useCallback(async () => {
+    const fetchedScheduleData = await getScheduleData();
+
+    const teamScheduleData = formatTeamSchedule(
+      currentSchedule,
+      fetchedScheduleData
+    );
+    setTeamScheduleData(teamScheduleData);
+  }, [currentSchedule]);
+
+  useEffect(() => {
+    fetchSchedules();
+  }, [fetchSchedules]);
   return <S.Box>DetailSchedule</S.Box>;
 };
 
