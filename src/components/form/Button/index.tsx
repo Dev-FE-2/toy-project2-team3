802a60ef59380370549d418c2a104b82670b4dac
gold <goldegg127@gmail.com>
2024-12-12 22:23:45 +0900

    feat: input 관련 컴포넌트 생성 및 form 디렉토리로 그룹화 (#57)

--- src/components/form/Button/index.tsx
+++ src/components/form/Button/index.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { colors, font } from '../../styles';
+import { colors, font } from '../../../styles';
 
 export const StyledCancelButton = styled.div`
   background-color: ${colors.semantic.danger};
