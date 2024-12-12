40f20303305cf4b0deba8ea54b50180eae4d9895
vgotu99 <vgotu99@gmail.com>
2024-12-12 18:49:50 +0900

    feat: fetchDataFromDB가 반환하는 값에 id: 테이블의 key를 추가(#65)

--- src/firebase/fetchDataFromDB.ts
+++ src/firebase/fetchDataFromDB.ts
@@ -1,7 +1,7 @@
 import { ref, get, DatabaseReference } from 'firebase/database';
 import { database } from '../firebaseConfig';
 
-type FetchData<T> = Promise<T | Record<string, T> | null>;
+type FetchData<T> = Promise<T[] | null>;
 
 interface FetchDataParams {
   table: string;
@@ -21,30 +21,28 @@ const fetchDataFromDB = async <T>({
 
     if (snapshot.exists()) {
       const data = snapshot.val();
-      const result = data ? (!key ? Object.values(data) : data) : null;
+      const result = Object.entries(data).map(([key, value]) => {
+        if (typeof value === 'object' && value !== null) {
+          return {
+            id: key,
+            ...value,
+          } as T;
+        }
+        return {
+          id: key,
+          value,
+        } as T;
+      });
 
       return result;
     } else {
       console.warn('데이터가 존재하지 않습니다.');
-
       return null;
     }
   } catch (error) {
     console.error('데이터 가져오기 실패:', error);
-
     throw error;
   }
 };
 
 export { fetchDataFromDB };
-
-/** 사용 방법
- * await fetchDataFromDB({ table, key });
- * ex) await fetchDataFromDB({ 'Users', 'p0qTmHR3PU1LJ3' });
- *
- * 매개변수는 { table, key } 객체 형태
- *
- * table의 값은 firebase realtime database의 collection name이다.
- * key의 값은 collection의 하위 데이터를 분류하는 문서 이름 혹은 null이 들어갈 수 있다. (optional)
- * key를 null로 전달할 경우 'Users'의 모든 데이터를 가져온다. 반환 값 : {} 형태
- */
