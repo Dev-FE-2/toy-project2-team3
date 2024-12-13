605274777cf5e9897e587bf5fc7b101432c96f1f
gold <goldegg127@gmail.com>
2024-12-13 15:52:54 +0900

    hotfix: 버튼 컴포넌트의 위치와 상관없이 export 가능하도록 수정 (#73)

--- src/pages/user/SalaryCorrectionPage/SalaryCorrectionHeader/index.tsx
+++ src/pages/user/SalaryCorrectionPage/SalaryCorrectionHeader/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { StyleCorrectiondButton } from '../../../../components/Button';
+import { StyleCorrectiondButton } from '../../../../components';
 
 type SalaryCorrectionHeaderProps = {
   isVisible: boolean;
