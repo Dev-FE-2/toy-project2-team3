8afaffb80e3307b77335e02bf800fdf9f8543b49
gold <goldegg127@gmail.com>
2024-12-13 20:11:02 +0900

    design: 공통 Form UI 컴포넌트 생성 및 적용 (#58)

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -2,4 +2,5 @@ export { default as Input } from './Input';
 export { default as InputFile } from './InputFile';
 export { default as Textarea } from './Textarea';
 export { default as ErrorMessage } from './ErrorMessage';
+export { default as Form } from './Form';
 export * from './Button';
