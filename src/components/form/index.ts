d86649f9523c8211e5c1fff6beb1c14d3a8b33b0
vgotu99 <vgotu99@gmail.com>
2024-12-14 00:40:32 +0900

    feat: 버튼 컴포넌트 구현(#80)

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -2,4 +2,4 @@ export { default as Input } from './Input';
 export { default as InputFile } from './InputFile';
 export { default as Textarea } from './Textarea';
 export { default as ErrorMessage } from './ErrorMessage';
-export * from './Button';
+export { default as Button } from './Button';
