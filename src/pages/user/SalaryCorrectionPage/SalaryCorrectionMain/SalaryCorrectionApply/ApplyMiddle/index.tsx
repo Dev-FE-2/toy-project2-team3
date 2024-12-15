a018adceed9e29da3e66701424b4f79e02936512
gold <goldegg127@gmail.com>
2024-12-16 02:52:15 +0900

    design: 한글자씩 줄바꿈이 되는 필드 텍스트 문제 해결 (#90)

--- src/pages/user/SalaryCorrectionPage/SalaryCorrectionMain/SalaryCorrectionApply/ApplyMiddle/index.tsx
+++ src/pages/user/SalaryCorrectionPage/SalaryCorrectionMain/SalaryCorrectionApply/ApplyMiddle/index.tsx
@@ -382,8 +382,9 @@ const S = {
   `,
   ApplyMiddleRow: styled.div`
     display: flex;
-    justify-content: row;
     align-items: center;
+    word-break: keep-all;
+    white-space: nowrap;
   `,
   RecordList: styled.div`
     margin-top: 10px;
