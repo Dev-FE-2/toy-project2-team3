4b29f0c7665aca9ca040b7f2845593eae9cf83d2
gold <goldegg127@gmail.com>
2024-12-15 21:40:16 +0900

    design: 페이지 UI 일관성을 위한 Layout 컴포넌트 구조 업데이트 (#90)

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,14 +1,18 @@
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
-// import { colors, border } from "../../../styles";
+import { padding, font } from '../../styles';
 import { default as SideBar } from './SideBar';
+// import { PageContainer, PageTitle } from "../../components";
 
 const Layout = () => {
   return (
     <S.Layout>
       <SideBar />
       <main>
-        <Outlet /> {/* 메인 콘텐츠 */}
+        <section className="page-container">
+          <h1 className="page-title">타이틀</h1>
+          <Outlet /> {/* 메인 콘텐츠 */}
+        </section>
       </main>
     </S.Layout>
   );
@@ -17,6 +21,33 @@ const Layout = () => {
 const S = {
   Layout: styled.div`
     display: flex;
+
+    main {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      .page-container {
+        width: 100%;
+        max-width: 1400px;
+        padding: ${padding.xl} ${padding.md};
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        gap: ${padding.xl};
+
+        @media (max-width: 768px) {
+        }
+      }
+
+      .page-title {
+        font-size: ${font.size.heading};
+        line-height: 1;
+        font-weight: ${font.weight.heading};
+      }
+    }
   `,
 };
 
