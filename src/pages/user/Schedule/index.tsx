363e1626c8a03bc4f3ab819f294dda7baeb0c1b9
vgotu99 <vgotu99@gmail.com>
2024-12-16 01:42:27 +0900

    refactor: 전역상태, 타입을 이용해서 Schedule page 리팩토링 (#61)

--- src/pages/user/Schedule/index.tsx
+++ src/pages/user/Schedule/index.tsx
@@ -2,81 +2,30 @@ import styled from 'styled-components';
 import ScheduleHeader from './ScheduleHeader';
 import ScheduleMain from './ScheduleMain';
 import ScheduleSideBar from './ScheduleSideBar';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { fetchDataFromDB } from '../../../firebase/fetchDataFromDB';
 import ScheduleModal from './core/ScheduleModal';
 import { useFetchUserInfo } from '../../../hooks';
 import { Loading } from '../../../components';
-
-interface TeamData {
-  id: string;
-  name: string;
-  members: TeamMembersData[];
-}
-
-interface TeamMembersData {
-  name: string;
-  userId: string;
-  number: number;
-}
-
-interface CurrentSchedule {
-  type: string;
-  teamId: TeamMembersData[];
-  userId?: string;
-}
-
-interface ScheduleList {
-  createdAt: string;
-  detail: string;
-  endedAt: string;
-  startedAt: string;
-  title: string;
-  updatedAt: string;
-}
-
-interface TargetSchedule extends ScheduleList {
-  id: string;
-  index: number;
-  name: string;
-  userId: string;
-  documentName: string;
-  documentUrl: string;
-}
-
-type ModalType = 'C' | 'R' | 'U' | 'D';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../../state/store';
+import {
+  setCurrentSchedule,
+  setTeamData,
+} from '../../../slices/schedule/scheduleSlice';
+import { TeamData, TeamMembersData } from './core/schedule';
 
 const Schedule = () => {
-  const [targetSchedule, setTargetSchedule] = useState<TargetSchedule>({
-    id: '',
-    index: 0,
-    createdAt: '',
-    detail: '',
-    endedAt: '',
-    startedAt: '',
-    title: '',
-    updatedAt: '',
-    name: '',
-    userId: '',
-    documentName: '',
-    documentUrl: '',
-  });
-  const [modalType, setModalType] = useState<ModalType>('C');
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [teamData, setTeamData] = useState<TeamData[]>([]);
-  const [currentSchedule, setCurrentSchedule] = useState<CurrentSchedule>({
-    type: '',
-    teamId: [],
-    userId: '',
-  });
-  const [isDayClick, setIsDayClick] = useState(false);
-  const [clickedDate, setClickedDate] = useState<number[]>([]);
-  const { userInfo, error, isLoading } = useFetchUserInfo();
+  const dispatch = useDispatch();
+  const { teamData, isModalOpen } = useSelector(
+    (state: RootState) => state.schedule
+  );
+  const { userInfo, isLoading, error } = useFetchUserInfo();
 
   const getTeamsData = async () => {
     const teamsData = (await fetchDataFromDB({ table: 'Teams' })) as TeamData[];
 
-    setTeamData(teamsData);
+    dispatch(setTeamData(teamsData));
 
     return teamsData;
   };
@@ -88,22 +37,18 @@ const Schedule = () => {
       )
     )?.members as TeamMembersData[];
 
-    setCurrentSchedule({
-      type: 'team',
-      teamId: currentUserTeams,
-      userId: '',
-    });
+    dispatch(
+      setCurrentSchedule({
+        type: 'team',
+        teamId: currentUserTeams,
+        userId: '',
+      })
+    );
   };
 
   useEffect(() => {
     getCurrentUserTeamsData();
   }, [userInfo]);
-
-  const handleRModalOpen = (targetSchedule: TargetSchedule) => {
-    setModalType('R');
-    setTargetSchedule(targetSchedule);
-    setIsModalOpen(true);
-  };
 
   useEffect(() => {
     getTeamsData();
@@ -118,23 +63,9 @@ const Schedule = () => {
         <ScheduleSideBar />
         <div>
           <ScheduleHeader />
-          <ScheduleMain
-            currentSchedule={currentSchedule}
-            isDayClick={isDayClick}
-            clickedDate={clickedDate}
-            setIsDayClick={setIsDayClick}
-            setClickedDate={setClickedDate}
-            handleRModalOpen={handleRModalOpen}
-          />
+          <ScheduleMain />
         </div>
-        {isModalOpen && (
-          <ScheduleModal
-            targetSchedule={targetSchedule}
-            modalType={modalType}
-            setModalType={setModalType}
-            setIsModalOpen={setIsModalOpen}
-          />
-        )}
+        {isModalOpen && <ScheduleModal />}
       </S.Wrapper>
     </>
   );
