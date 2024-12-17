bcbe4b7f15631c8a07d16bb15e76dee516c16cc7
gold <goldegg127@gmail.com>
2024-12-17 12:29:51 +0900

    fix: resolve conflict develop (#106)

--- src/pages/user/index.ts
+++ src/pages/user/index.ts
@@ -1,6 +1,6 @@
 export { default as UserHomePage } from './UserHomePage';
 export { default as SchedulePage } from './SchedulePage';
-export { default as ProfilePage } from './ProfilePage';
+export { default as ProfilePage } from './Profile';
 export { default as AttendancePage } from './AttendancePage';
 export { default as SalaryCorrectionPage } from './SalaryCorrectionPage';
 export { default as SalaryDetailsPage } from './SalaryDetailsPage';
