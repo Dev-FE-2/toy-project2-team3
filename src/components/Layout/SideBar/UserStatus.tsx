054151ec37d72eb48478e72118395429bf43ecbb
gold <goldegg127@gmail.com>
2024-12-19 01:00:51 +0900

    design: 디자인 디벨롭을 위해 UserStatus 컴포넌트에서 로그아웃 버튼 제거 (#104)

--- src/components/Layout/SideBar/UserStatus.tsx
+++ src/components/Layout/SideBar/UserStatus.tsx
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 import type { RootState } from '../../../state/store';
 import { colors, font, padding } from '../../../styles';
 import UserProfilePhoto from '../../UserProfilePhoto';
-import LoggoutButton from './LoggoutButton';
 import defaultImage from 'public/avatar.svg';
 
 type UserStatusProps = {
@@ -26,42 +25,33 @@ const UserStatus = ({ style }: UserStatusProps) => {
   };
 
   return isLoggedIn ? (
-    <S.UserStatusWrap padding={padding}>
-      <S.UserStatus>
-        <UserProfilePhoto
-          width="50"
-          imageUrl={userInfo.profileImgUrl ?? defaultImage}
-          userName={userInfo.name ?? '알 수 없는 사용자'}
-          onClick={handleProfile}
-        />
-        <div className="user-state">
-          <span className="user-name">
-            {userInfo.name} {userInfo.position}
-          </span>
-          <span className="state-info">
-            {userInfo.team} {userInfo.department}
-          </span>
-        </div>
-      </S.UserStatus>
-      <LoggoutButton />
-    </S.UserStatusWrap>
+    <S.UserStatus padding={padding}>
+      <UserProfilePhoto
+        width="50"
+        imageUrl={userInfo.profileImgUrl ?? defaultImage}
+        userName={userInfo.name ?? '알 수 없는 사용자'}
+        onClick={handleProfile}
+      />
+      <div className="user-state">
+        <span className="user-name">
+          {userInfo.name} {userInfo.position}
+        </span>
+        <span className="state-info">
+          {userInfo.team} {userInfo.department}
+        </span>
+      </div>
+    </S.UserStatus>
   ) : (
     <p>사용자 정보가 없습니다.</p>
   );
 };
 
 const S = {
-  UserStatusWrap: styled.article<UserStatusProps['style']>`
+  UserStatus: styled.article<{ padding: string }>`
     display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: ${(props) => props.padding};
-  `,
-  UserStatus: styled.article`
-    display: flex;
-    justify-content: space-between;
     align-items: center;
     gap: ${padding.md};
+    padding: ${(props) => props.padding};
 
     .user-state {
       display: flex;
@@ -71,19 +61,12 @@ const S = {
 
       .user-name {
         font-weight: 500;
+        color: ${colors.semantic.text.dark};
       }
 
       .state-info {
         font-size: ${font.size.info};
-        color: ${colors.semantic.info};
       }
-    }
-  `,
-  LoggoutButton: styled.button`
-    .material-symbols-outlined {
-      font-size: 42px; // 아이콘 크기 조정
-      margin-left: 10px; // 왼쪽 여백 추가
-      cursor: pointer; // 커서 변경
     }
   `,
 };
