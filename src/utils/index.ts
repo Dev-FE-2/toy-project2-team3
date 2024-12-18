ee76b7f6ce556f87c25553405111fcaa5aa9b800
vgotu99 <vgotu99@gmail.com>
2024-12-18 14:49:30 +0900

    rename: fetchUserInfo 확장자를 tsx -> ts로 변경, src/firebase -> src/utils로 경로 변경 (#102)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,7 @@ export * from './format';
 export * from './generate';
 export * from './uploadFile';
 export * from './navigate';
+export * from './saveDataToDB';
+export * from './fetchDataFromDB';
+export * from './fetchUserInfo';
+export * from './assignColor';
