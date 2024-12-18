31374d9a70ed4e11262afc5457f44e5564ed147e
gold <goldegg127@gmail.com>
2024-12-19 01:14:34 +0900

    chore: 유틸함수 중 JS Doc 파라미터 누락 수정 (#104)

--- src/utils/navigate.ts
+++ src/utils/navigate.ts
@@ -5,7 +5,8 @@ import { URL } from '../constant';
 /**
  * getPageTitle - path 문자열을 인자로 넘기고 해당 경로의 페이지 타이틀을 반환 합니다.
  *
- * @returns {string} useLocation().pathname, widdow.location.pathname 반환 값과 같은 경로 ex) /admin/salaryRequest
+ * @param {string} path useLocation().pathname, widdow.location.pathname 반환 값과 같은 경로 ex) /admin/salaryRequest
+ * @returns {string}
  */
 export const getPageTitle = (path: string): string => {
   for (const key in URL) {
