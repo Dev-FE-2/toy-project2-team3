cfedce7caefae782d6def73ba376be3bab74c826
Chajaesik01 <127061507+Chajaesik01@users.noreply.github.com>
2024-12-19 14:25:15 +0900

    [Refactor] 전역 상태 pagination을 제거하고 query-parameter를 적용 (#117)
    * refactor: pagination constant 추가
    
    * refactor: useUserInfo 훅 대신 전역 state 사용
    
    * refactor: pagination query-parameter적용 (#110)
    
    * refactor: 컴포넌트 파일 구조에 맞게 폴더 구조 수정

--- src/constant/pagination.ts
+++ src/constant/pagination.ts
@@ -1,2 +1,1 @@
-export const VISIBLE_PAGE_COUNT = 10;
 export const ITEM_PER_PAGE = 10;
--- src/pages/user/index.ts
+++ src/pages/user/index.ts
@@ -2,5 +2,5 @@ export { default as UserHomePage } from './UserHomePage';
 export { default as SchedulePage } from './SchedulePage';
 export { default as ProfilePage } from './Profile';
 export { default as AttendancePage } from './AttendancePage';
-export { default as SalaryCorrectionPage } from './SalaryCorrectionPage';
-export { default as SalaryDetailsPage } from './SalaryDetailsPage';
+export { default as SalaryCorrectionPage } from './SalaryCorrectionPage/SalaryCorrectionPage';
+export { default as SalaryDetailsPage } from './SalaryDetailsPage/SalaryDetailsPage';
