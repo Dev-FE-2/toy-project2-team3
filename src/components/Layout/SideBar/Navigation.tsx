4faedf4cb9791e3f45b4477c5b55fd2e0f7297af
gold <goldegg127@gmail.com>
2024-12-19 19:23:54 +0900

    refactor: 서브 NavItem 토글기능과 아이콘 추가 (#104)

--- src/components/Layout/SideBar/Navigation.tsx
+++ src/components/Layout/SideBar/Navigation.tsx
@@ -1,33 +1,156 @@
-// import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { useLogout } from '../../../hooks';
 import { hexToRgba } from '../../../utils';
 import { URL } from '../../../constant';
-import { colors } from '../../../styles';
+import { colors, padding } from '../../../styles';
+import { Icon } from '../../../components';
+
+type NavItem = {
+  icon: string;
+  text: string;
+  link: string;
+  name: string;
+  hasSubNav: boolean;
+  firstSubNav: string;
+  isSubNav: boolean;
+  parentNav: string;
+  iconSize?: number;
+};
+
+const getNavItems = (
+  navItem: Pick<NavItem, 'text' | 'link' | 'name'>,
+  {
+    hasSubNav,
+    firstSubNav,
+    isSubNav,
+    parentNav,
+    icon,
+    iconSize,
+  }: Omit<NavItem, 'text' | 'link' | 'name'>
+) => ({
+  ...navItem,
+  hasSubNav,
+  firstSubNav,
+  isSubNav,
+  parentNav,
+  icon,
+  iconSize,
+});
 
 const navItems = [
-  URL.userHome,
-  URL.profile,
-  URL.schedule,
-  URL.salary,
-  URL.salaryDetails,
-  URL.salaryCorrection,
+  getNavItems(URL.userHome, {
+    hasSubNav: false,
+    firstSubNav: '',
+    isSubNav: false,
+    parentNav: '',
+    icon: 'home',
+  }),
+  getNavItems(URL.profile, {
+    hasSubNav: false,
+    firstSubNav: '',
+    isSubNav: false,
+    parentNav: '',
+    icon: 'person',
+  }),
+  getNavItems(URL.schedule, {
+    hasSubNav: false,
+    firstSubNav: '',
+    isSubNav: false,
+    parentNav: '',
+    icon: 'calendar_today',
+    iconSize: 22,
+  }),
+  getNavItems(URL.salary, {
+    hasSubNav: true,
+    firstSubNav: `${URL.salaryDetails.link}`,
+    isSubNav: false,
+    parentNav: '',
+    icon: 'attach_money',
+  }),
+  getNavItems(URL.salaryDetails, {
+    hasSubNav: false,
+    firstSubNav: '',
+    isSubNav: true,
+    parentNav: `${URL.salary.link}`,
+    icon: '',
+  }),
+  getNavItems(URL.salaryCorrection, {
+    hasSubNav: false,
+    firstSubNav: '',
+    isSubNav: true,
+    parentNav: `${URL.salary.link}`,
+    icon: '',
+  }),
 ];
 
 const Navigation = ({ style }: { style: { padding: string } }) => {
+  const navigate = useNavigate();
   const { handleLogout } = useLogout();
+  const [isToggle, setToggle] = useState(true);
+
+  const handleToggle = (firstSubNav: string) => {
+    setToggle(!isToggle);
+    navigate(firstSubNav);
+  };
 
   return (
     <S.Navigation padding={style.padding}>
-      {navItems.map((navItem) => (
-        <S.NavItem as={NavLink} to={navItem.link}>
-          {navItem.text}
-        </S.NavItem>
-      ))}
+      {navItems.map((navItem) =>
+        navItem.hasSubNav ? (
+          <S.NavItem
+            onClick={() => handleToggle(navItem.firstSubNav)}
+            className={`has-sub-nav${isToggle ? ' selected' : ''}`}
+            isToggle={isToggle}
+          >
+            <NavText
+              text={navItem.text}
+              icon={navItem.icon}
+              iconSize={navItem.iconSize}
+            />
+            <Icon name="keyboard_arrow_down" />
+          </S.NavItem>
+        ) : !navItem.isSubNav ? (
+          <S.NavItem as={NavLink} to={navItem.link}>
+            <NavText
+              text={navItem.text}
+              icon={navItem.icon}
+              iconSize={navItem.iconSize}
+            />
+          </S.NavItem>
+        ) : isToggle ? (
+          <S.NavItem as={NavLink} to={navItem.link} className="sub-nav">
+            <NavText
+              text={navItem.text}
+              icon={navItem.icon}
+              iconSize={navItem.iconSize}
+            />
+          </S.NavItem>
+        ) : (
+          <></>
+        )
+      )}
       <hr />
       <S.NavItem onClick={handleLogout}>Log out</S.NavItem>
     </S.Navigation>
+  );
+};
+
+const NavText = ({
+  text,
+  icon,
+  iconSize,
+}: {
+  text: string;
+  icon: string;
+  iconSize?: number;
+}) => {
+  return (
+    <S.NavText>
+      {icon && <Icon name={icon} size={iconSize} />}
+      {text}
+    </S.NavText>
   );
 };
 
@@ -45,11 +168,12 @@ const S = {
       padding: 0 ${(props) => props.padding};
     }
   `,
-  NavItem: styled.div`
+  NavItem: styled.div<{ isToggle?: boolean }>`
     height: 44px;
     display: flex;
     align-items: center;
     cursor: pointer;
+    transition: color ease 0.2s;
 
     &.active {
       color: ${colors.semantic.primary};
@@ -59,10 +183,36 @@ const S = {
       color: ${colors.semantic.hover.primary};
     }
 
-    &.selected {
+    &.sub-nav {
+      margin-left: ${padding.lg};
+    }
+
+    &.has-sub-nav {
+      display: flex;
+      justify-content: space-between;
+    }
+
+    ${(props) =>
+      props.isToggle &&
+      `
+      .material-symbols-outlined {
+        transform: rotate(180deg);
+      }      
+    `}
+
+    &.has-sub-nav.selected {
       background-color: ${hexToRgba(colors.semantic.primary, 0.05)};
       border-right: 3px solid ${colors.semantic.primary};
     }
+
+    .nav-text {
+    }
+  `,
+  NavText: styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: ${padding.sm};
   `,
 };
 
